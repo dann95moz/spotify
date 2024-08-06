af44cda779a54a4f26b53c1aca8b77b1c69be3c3
Daniel Corredor <danielcasw@gmail.com>
2024-08-06 13:26:50 -0500

    routes organization

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,18 @@
-
-import { HomeComponent } from './components/home/home.component';
-import { DetailsComponent } from './components/details/details.component';
+import { DetailsComponent } from './pages/details/details.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CallbackComponent } from './components/callback/callback.component';
-import { LoggedComponent } from './components/logged/logged.component';
-import { PlaylistDetailsComponent } from './components/playlist-details/playlist-details.component';
+import { CallbackComponent } from './pages/callback/callback.component';
+import { LoggedComponent } from './pages/logged/logged.component';
+import { PlaylistDetailsComponent } from './pages/playlist-details/playlist-details.component';
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'details/:type/:id', component: DetailsComponent },
-    { path: 'callback', component: CallbackComponent },
-    { path: 'logged', component: LoggedComponent },
-    {path:'logged/playlist/:playlistId', component: PlaylistDetailsComponent},
-    { path: '**', redirectTo:''}
+  { path: 'details/:type/:id', component: DetailsComponent },
+  { path: 'callback', component: CallbackComponent },
+  { path: 'logged', component: LoggedComponent },
+  { path: 'logged/playlist/:playlistId', component: PlaylistDetailsComponent },
+  { path: '**', redirectTo: '' },
 ];
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  })
-  export class AppRoutingModule { }
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,16 +4,16 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
-import {MatProgressSpinner }from '@angular/material/progress-spinner';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { Subject, takeUntil } from 'rxjs';
 import { SpotifyService } from '../../services/api/spotify.service';
 import { AuthService } from '../../services/spotifyAuth/auth.service';
-import { PlayerService } from '../../services/player/player.service';
 import { MatIcon } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import {MatPaginatorModule, PageEvent} from '@angular/material/paginator';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { SpotifySearch } from '../../services/api/search.interface';
 interface SearchResult {
   id: string;
   type: string;
@@ -34,35 +34,34 @@ interface SearchResult {
     FormsModule,
     MatIcon,
     MatCardModule,
-    MatPaginatorModule
+    MatPaginatorModule,
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  
-  
 })
-export class HomeComponent implements OnDestroy  {
-  searchQuery = '';
+export class HomeComponent implements OnDestroy {
+  searchQuery: string = '';
   results: SearchResult[] = [];
-  isLoading = false;
+  isLoading: boolean = false;
   private destroy$ = new Subject<void>();
-  paginatedResults: any[] = [];
+  paginatedResults: SearchResult[] = [];
 
-  pageSize = 5;
-  constructor(private spotifyService: SpotifyService, private router: Router, private authService: AuthService) { 
+  pageSize: number = 5;
+  constructor(
+    private spotifyService: SpotifyService,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
-  }
- 
   login() {
-    
     this.authService.login();
   }
   onSearch(): void {
     if (!this.searchQuery.trim()) return;
 
-    
     this.isLoading = true;
-    this.spotifyService.search(this.searchQuery)
+    this.spotifyService
+      .search(this.searchQuery)
       .pipe(takeUntil(this.destroy$))
       .subscribe({
         next: (response) => {
@@ -73,26 +72,24 @@ export class HomeComponent implements OnDestroy  {
         error: (error) => {
           console.error('Error searching:', error);
           this.isLoading = false;
-         //maneja los errores
-        }
+          //maneja los errores
+        },
       });
   }
   onPageChange(event: PageEvent): void {
-    const startIndex = event.pageIndex * event.pageSize;
-    const endIndex = startIndex + event.pageSize;
+    const startIndex: number = event.pageIndex * event.pageSize;
+    const endIndex: number = startIndex + event.pageSize;
     this.paginatedResults = this.results.slice(startIndex, endIndex);
   }
-  private processSearchResults(response: any): SearchResult[] {
-    
+  private processSearchResults(response: SpotifySearch): SearchResult[] {
     const tracks = response.tracks?.items || [];
     const artists = response.artists?.items || [];
     const albums = response.albums?.items || [];
 
-    return [...tracks, ...artists, ...albums].map(item => ({
+    return [...tracks, ...artists, ...albums].map((item) => ({
       id: item.id,
       type: item.type,
       name: item.name,
-  
     }));
   }
 
