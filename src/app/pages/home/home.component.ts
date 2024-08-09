bd00f0790746a4034f90b5c5ff89fbb2748d2e3a
Daniel Corredor <dann95.moz@gmail.com>
2024-08-09 18:22:09 -0500

    minimize home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,44 +1,20 @@
 import { Component } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
-import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { finalize, Observable, tap } from 'rxjs';
-import { MatIcon } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { AsyncPipe } from '@angular/common';
 import { LoginBtnComponent } from '@components/login-btn/login-btn.component';
 import { SearchSongFormComponent } from '@components/searchSongForm/search-song-form.component';
 import { PaginatorComponent } from '@components/paginator/paginator.component';
-import { CardListComponent } from "../../components/card-list/card-list.component";
-interface SearchResult {
-  id: string;
-  type: string;
-  name: string;
-  // Añade más propiedades según la estructura de los resultados de Spotify
-}
+import { CardListComponent } from "@components/card-list/card-list.component";
+import { SearchResult } from '@interfaces/searchResult';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    MatToolbarModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatListModule,
-    MatButtonModule,
-    MatProgressSpinner,
-    FormsModule,
-    MatIcon,
-    MatCardModule,
-    MatPaginatorModule,
-    SearchSongFormComponent,
     AsyncPipe,
+    MatProgressSpinner,
+    SearchSongFormComponent,
     LoginBtnComponent,
     PaginatorComponent,
     CardListComponent
@@ -47,19 +23,15 @@ interface SearchResult {
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent   {
-  constructor(
-    private router: Router,
-   
-  ) {}
   isLoading: boolean = false;
   searchResults$: Observable<SearchResult[]> | null = null;
   searchQuery: string = '';
   results: SearchResult[] = [];
   paginatedResults: SearchResult[] = [];
+  pageSize: number = 5;
   onPaginatedResults(paginatedResults: SearchResult[]) {
     this.paginatedResults= paginatedResults
   }
-  pageSize: number = 5;
   handleSearch(results$: Observable<SearchResult[]>) {
     this.isLoading = true
     this.searchResults$ = results$.pipe(
@@ -68,12 +40,5 @@ export class HomeComponent   {
         this.paginatedResults = response.slice(0, this.pageSize);
       }),
     finalize(()=>this.isLoading= false));
-
 }
-
-  viewDetails(item: SearchResult): void {
-    this.router.navigate(['/details', item.type, item.id]);
-  }
- 
-
 }
