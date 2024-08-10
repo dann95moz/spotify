f4d88534559ec8503ad896d764005eb24f924c96
Daniel Corredor <dann95.moz@gmail.com>
2024-08-10 10:07:34 -0500

    remove unused

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
-import { finalize, Observable, tap } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { LoginBtnComponent } from '@components/login-btn/login-btn.component';
 import { SearchSongFormComponent } from '@components/searchSongForm/search-song-form.component';
 import { PaginatorComponent } from '@components/paginator/paginator.component';
 import { CardListComponent } from "@components/card-list/card-list.component";
-import { SearchResult } from '@interfaces/searchResult';
 import { PaginatedCardListComponent } from '@components/paginated-card-list/paginated-card-list.component';
 
 @Component({
