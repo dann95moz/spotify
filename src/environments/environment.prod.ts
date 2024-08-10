0a8de1117bedf4a1d738b6cc0e1536d9eb999961
Daniel Corredor <dann95.moz@gmail.com>
2024-08-10 10:16:39 -0500

    api spotify on environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   spotifyClientId: process.env['SPOTIFY_CLIENT_ID'],
   spotifyClientSecret: process.env['SPOTIFY_CLIENT_SECRET'],
+  spotifyApi: 'https://api.spotify.com/v1',
   redirectUri: 'http://localhost:4200', //TODO: assign real URI
 };
